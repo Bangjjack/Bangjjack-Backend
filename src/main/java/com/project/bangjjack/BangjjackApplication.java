c6bbc179d6426cbaf299521c4bd174b8703dd18d
1winhyun <jimking1@naver.com>
2026-04-09 12:51:43 +0900

    feat: 프로젝트 초기 세팅

---
+++ src/main/java/com/project/bangjjack/BangjjackApplication.java
@@ -0,0 +1,13 @@
+package com.project.bangjjack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BangjjackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BangjjackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/bangjjack/BangjjackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.bangjjack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BangjjackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
