dba1ce04682267d17e5d22c95dc7082070543b56
xEzIxX <by1094@gachon.ac.kr>
2026-05-06 00:58:53 +0900

    docs: 룸메이트 모집글 상태 문구 및 명세서에서 해당 부분 수정

--- src/main/java/com/project/bangjjack/domain/post/application/exception/PostErrorCode.java
+++ src/main/java/com/project/bangjjack/domain/post/application/exception/PostErrorCode.java
@@ -9,7 +9,7 @@ import org.springframework.http.HttpStatus;
 @AllArgsConstructor
 public enum PostErrorCode implements ErrorCodeInterface {
 
-    ALREADY_OPEN_POST_EXISTS(40701, HttpStatus.CONFLICT, "이미 작성 중인 룸메이트 모집글이 존재합니다."),
+    ALREADY_OPEN_POST_EXISTS(40701, HttpStatus.CONFLICT, "이미 모집 중인 룸메이트 모집글이 존재합니다."),
     POST_WRITE_PRECONDITION_NOT_MET(40702, HttpStatus.CONFLICT, "모집글 작성 전 온보딩, 체크리스트, 선호도 등록을 완료해주세요."),
     INVALID_RECRUIT_MEMBER_COUNT(40703, HttpStatus.BAD_REQUEST, "모집 인원이 방 유형과 일치하지 않습니다.");
 
