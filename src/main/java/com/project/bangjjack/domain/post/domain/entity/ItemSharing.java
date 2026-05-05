22657c6b4c062273a30bf621ba4285381cba359c
xEzIxX <by1094@gachon.ac.kr>
2026-05-05 20:38:53 +0900

    feat: 공동 생활습관 entity 추가

---
+++ src/main/java/com/project/bangjjack/domain/post/domain/entity/ItemSharing.java
@@ -0,0 +1,5 @@
+package com.project.bangjjack.domain.post.domain.entity;
+
+public enum ItemSharing {
+    USE_SEPARATELY, BORROW_WITH_PERMISSION, NO_PREFERENCE
+}
---
+++ src/main/java/com/project/bangjjack/domain/post/domain/entity/LightsOutTime.java
@@ -0,0 +1,5 @@
+package com.project.bangjjack.domain.post.domain.entity;
+
+public enum LightsOutTime {
+    BEFORE_23, BETWEEN_23_24, BETWEEN_00_01, BETWEEN_01_02, AFTER_03, NO_PREFERENCE
+}
