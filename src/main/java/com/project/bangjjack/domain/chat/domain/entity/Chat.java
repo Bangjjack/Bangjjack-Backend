2a118a56b06976042a37485253befccf96e3cf90
xEzIxX <by1094@gachon.ac.kr>
2026-05-08 14:08:29 +0900

    feat: Chat entity 추가

---
+++ src/main/java/com/project/bangjjack/domain/chat/domain/entity/Chat.java
@@ -0,0 +1,33 @@
+package com.project.bangjjack.domain.chat.domain.entity;
+
+import com.project.bangjjack.global.common.entity.BaseEntity;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(
+    name = "chat",
+    indexes = @Index(name = "IDX_chat_room_id_id_desc", columnList = "room_id, id DESC")
+)
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+public class Chat extends BaseEntity {
+
+    @Column(name = "user_id", nullable = false)
+    private Long userId;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "room_id", nullable = false)
+    private ChatRoom chatRoom;
+
+    @Column(nullable = false, columnDefinition = "TEXT")
+    private String content;
+
+    public static Chat create(Long userId, ChatRoom chatRoom, String content) {
+        return new Chat(userId, chatRoom, content);
+    }
+}
