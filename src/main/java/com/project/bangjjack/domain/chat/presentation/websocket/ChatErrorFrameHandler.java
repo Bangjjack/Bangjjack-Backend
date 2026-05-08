ff822e9dc9b3914a1c8a3f439d581d082c48c05b
xEzIxX <by1094@gachon.ac.kr>
2026-05-08 16:30:18 +0900

    refactor: STOMP 에러 핸들러를 빈 생성 이후 끼워넣는 방식에서 설정 단계에 포함하는 방식으로 변경

---
+++ src/main/java/com/project/bangjjack/domain/chat/presentation/websocket/ChatErrorFrameHandler.java
@@ -0,0 +1,35 @@
+package com.project.bangjjack.domain.chat.presentation.websocket;
+
+import com.project.bangjjack.domain.chat.application.exception.ChatRoomErrorCode;
+import com.project.bangjjack.global.common.exception.ApplicationException;
+import com.project.bangjjack.global.common.exception.ErrorCodeInterface;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.simp.stomp.StompCommand;
+import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
+import org.springframework.messaging.support.MessageBuilder;
+import org.springframework.web.socket.messaging.StompSubProtocolErrorHandler;
+
+@Slf4j
+public class ChatErrorFrameHandler extends StompSubProtocolErrorHandler {
+
+    @Override
+    public Message<byte[]> handleClientMessageProcessingError(
+            Message<byte[]> clientMessage, Throwable ex) {
+
+        Throwable cause = ex.getCause() != null ? ex.getCause() : ex;
+        if (cause instanceof ApplicationException appEx) {
+            return buildErrorFrame(appEx.getErrorCode());
+        }
+        log.error("STOMP 처리 중 예상치 못한 오류 발생", cause);
+        return buildErrorFrame(ChatRoomErrorCode.CHAT_SERVER_ERROR);
+    }
+
+    private Message<byte[]> buildErrorFrame(ErrorCodeInterface errorCode) {
+        StompHeaderAccessor headers = StompHeaderAccessor.create(StompCommand.ERROR);
+        headers.setMessage(errorCode.getMessage());
+        headers.setNativeHeader("code", String.valueOf(errorCode.getCode()));
+        headers.setLeaveMutable(true);
+        return MessageBuilder.createMessage(new byte[0], headers.getMessageHeaders());
+    }
+}
--- src/main/java/com/project/bangjjack/global/config/websocket/WebSocketConfig.java
+++ src/main/java/com/project/bangjjack/global/config/websocket/WebSocketConfig.java
@@ -1,18 +1,22 @@
 package com.project.bangjjack.global.config.websocket;
 
+import com.project.bangjjack.domain.chat.presentation.websocket.ChatErrorFrameHandler;
 import com.project.bangjjack.domain.chat.presentation.websocket.StompAuthChannelInterceptor;
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.ChannelRegistration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
-import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.messaging.support.AbstractSubscribableChannel;
+import org.springframework.web.socket.WebSocketHandler;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
-import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurationSupport;
+import org.springframework.web.socket.messaging.StompSubProtocolHandler;
+import org.springframework.web.socket.messaging.SubProtocolWebSocketHandler;
 
 @Configuration
-@EnableWebSocketMessageBroker
 @RequiredArgsConstructor
-public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+public class WebSocketConfig extends WebSocketMessageBrokerConfigurationSupport {
 
     private final StompAuthChannelInterceptor stompAuthChannelInterceptor;
 
@@ -32,5 +36,20 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void configureClientInboundChannel(ChannelRegistration registration) {
         registration.interceptors(stompAuthChannelInterceptor);
+    }
+
+    @Bean
+    @Override
+    public WebSocketHandler subProtocolWebSocketHandler(
+            AbstractSubscribableChannel clientInboundChannel,
+            AbstractSubscribableChannel clientOutboundChannel) {
+        SubProtocolWebSocketHandler handler =
+                (SubProtocolWebSocketHandler) super.subProtocolWebSocketHandler(clientInboundChannel, clientOutboundChannel);
+        handler.getProtocolHandlers().stream()
+                .filter(h -> h instanceof StompSubProtocolHandler)
+                .map(h -> (StompSubProtocolHandler) h)
+                .findFirst()
+                .ifPresent(h -> h.setErrorHandler(new ChatErrorFrameHandler()));
+        return handler;
     }
 }
