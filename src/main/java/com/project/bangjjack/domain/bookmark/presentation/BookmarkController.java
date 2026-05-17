051e884816519c89e2b32de6e1e3128f3a48e3f8
xEzIxX <by1094@gachon.ac.kr>
2026-05-18 00:13:37 +0900

    feat: 내 북마크 목록 조회 controller 추가

--- src/main/java/com/project/bangjjack/domain/bookmark/presentation/BookmarkController.java
+++ src/main/java/com/project/bangjjack/domain/bookmark/presentation/BookmarkController.java
@@ -1,12 +1,16 @@
 package com.project.bangjjack.domain.bookmark.presentation;
 
+import com.project.bangjjack.domain.bookmark.application.dto.response.BookmarkedPostResponse;
 import com.project.bangjjack.domain.bookmark.application.usecase.BookmarkUseCase;
 import com.project.bangjjack.domain.bookmark.presentation.response.BookmarkResponseCode;
 import com.project.bangjjack.global.annotation.CurrentMemberId;
 import com.project.bangjjack.global.common.response.CommonResponse;
+import com.project.bangjjack.global.common.response.SliceResponse;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
@@ -36,5 +40,16 @@ public class BookmarkController {
             @PathVariable Long postId) {
         bookmarkUseCase.deleteBookmark(memberId, postId);
         return CommonResponse.success(BookmarkResponseCode.BOOKMARK_DELETED);
+    }
+
+    @Operation(summary = "내 북마크 목록 조회", description = "북마크한 모집글 목록을 최근 북마크순으로 조회합니다.")
+    @GetMapping("/bookmarks")
+    public CommonResponse<SliceResponse<BookmarkedPostResponse>> getBookmarkedPosts(
+            @CurrentMemberId Long memberId,
+            @PageableDefault(size = 20) Pageable pageable) {
+        return CommonResponse.success(
+                BookmarkResponseCode.BOOKMARKED_POST_LIST_FOUND,
+                bookmarkUseCase.getBookmarkedPosts(memberId, pageable)
+        );
     }
 }
