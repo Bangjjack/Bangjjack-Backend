765aa8006d1b9c157ac559e9e3532b3cf6245bdc
xEzIxX <by1094@gachon.ac.kr>
2026-05-18 01:00:37 +0900

    refactor: 북마크 에러 코드 prefix 409xx로 변경 및 관련 명세서 동기화

--- src/main/java/com/project/bangjjack/domain/bookmark/application/exception/BookmarkErrorCode.java
+++ src/main/java/com/project/bangjjack/domain/bookmark/application/exception/BookmarkErrorCode.java
@@ -9,8 +9,8 @@ import org.springframework.http.HttpStatus;
 @AllArgsConstructor
 public enum BookmarkErrorCode implements ErrorCodeInterface {
 
-    ALREADY_BOOKMARKED(40801, HttpStatus.CONFLICT, "이미 북마크한 게시글입니다."),
-    BOOKMARK_NOT_FOUND(40802, HttpStatus.NOT_FOUND, "북마크를 찾을 수 없습니다.");
+    ALREADY_BOOKMARKED(40901, HttpStatus.CONFLICT, "이미 북마크한 게시글입니다."),
+    BOOKMARK_NOT_FOUND(40902, HttpStatus.NOT_FOUND, "북마크를 찾을 수 없습니다.");
 
     private final int code;
     private final HttpStatus status;
