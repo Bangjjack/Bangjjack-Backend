e3cfaef14b9d46ef964cc09ce6de632785698299
xEzIxX <by1094@gachon.ac.kr>
2026-05-20 11:12:27 +0900

    feat: 메세지 타입 enum 추가

---
+++ src/main/java/com/project/bangjjack/domain/chat/domain/entity/MessageType.java
@@ -0,0 +1,10 @@
+package com.project.bangjjack.domain.chat.domain.entity;
+
+public enum MessageType {
+    USER,
+    APPLICATION_SENT,
+    APPLICATION_ACCEPTED,
+    APPLICATION_REJECTED,
+    GROUP_DISBANDED,
+    SYSTEM
+}
