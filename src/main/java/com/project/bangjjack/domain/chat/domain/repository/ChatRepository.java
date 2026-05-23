75003a557ea7e89e006f1f902c677607ed4e16e7
xEzIxX <by1094@gachon.ac.kr>
2026-05-23 16:33:11 +0900

    feat: 메세지 배치 조회 추가

--- src/main/java/com/project/bangjjack/domain/chat/domain/repository/ChatRepository.java
+++ src/main/java/com/project/bangjjack/domain/chat/domain/repository/ChatRepository.java
@@ -6,10 +6,15 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import java.util.Collection;
 import java.util.List;
 
 public interface ChatRepository extends JpaRepository<Chat, Long> {
 
     @Query("SELECT c FROM Chat c WHERE c.chatRoom.id = :roomId AND c.id < :cursorId AND c.deleted = false ORDER BY c.id DESC")
     List<Chat> findByCursorPage(@Param("roomId") Long roomId, @Param("cursorId") Long cursorId, Pageable pageable);
+
+    @Query("SELECT c FROM Chat c WHERE c.deleted = false AND c.id IN (SELECT MAX(c2.id) FROM Chat c2 WHERE c2.chatRoom.id IN :roomIds AND c2.deleted = false GROUP BY c2.chatRoom.id)")
+    List<Chat> findLastMessagesByRoomIds(@Param("roomIds") Collection<Long> roomIds);
+
 }
