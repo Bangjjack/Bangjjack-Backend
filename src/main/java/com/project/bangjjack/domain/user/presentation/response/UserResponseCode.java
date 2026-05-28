d088dfc7e27a9c9da70b40ddece201f5e77113c8
1winhyun <jimking1@naver.com>
2026-05-29 00:27:56 +0900

    feat: 프로필 수정 기능 controller 구현

--- src/main/java/com/project/bangjjack/domain/user/presentation/response/UserResponseCode.java
+++ src/main/java/com/project/bangjjack/domain/user/presentation/response/UserResponseCode.java
@@ -12,6 +12,7 @@ public enum UserResponseCode implements ResponseCodeInterface {
     ONBOARDING_COMPLETED(200, HttpStatus.OK, "온보딩이 완료되었습니다."),
     USER_BASIC_TAG_FOUND(200, HttpStatus.OK, "사용자 기본 태그 정보를 조회했습니다."),
     MY_PROFILE_FOUND(200, HttpStatus.OK, "내 프로필 정보를 조회했습니다."),
+    MY_PROFILE_UPDATED(200, HttpStatus.OK, "내 프로필이 수정되었습니다."),
     USER_PROFILE_FOUND(200, HttpStatus.OK, "사용자 프로필 정보를 조회했습니다."),
     AI_RECOMMENDED_ROOMMATES_FOUND(200, HttpStatus.OK, "AI 추천 룸메이트 목록을 조회했습니다.");
 
